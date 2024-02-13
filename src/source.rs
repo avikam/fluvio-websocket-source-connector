9812ff11bb04eb5f66fd5032b46372aa4f0387b3
avikam <avikam@gmail.com>
2024-02-12 23:12:36 -0500

    refactored static functions, and moved from channels to streams. Still missing ping-pongs

--- src/source.rs
+++ src/source.rs
@@ -3,17 +3,101 @@ use anyhow::{Result,Context};
 use async_trait::async_trait;
 use fluvio::Offset;
 use fluvio_connector_common::{tracing::{error, debug, info}, Source};
-use futures::{stream::{ LocalBoxStream, SplitStream, SplitSink }, StreamExt, SinkExt, self};
-use tokio::{net::TcpStream, time};
-use tokio::sync::mpsc::{self, Receiver, Sender};
+use futures::{self, stream::LocalBoxStream, SinkExt, StreamExt};
+use tokio::net::TcpStream;
 use tokio::time::{sleep, Duration};
-use tokio_stream::{StreamMap, wrappers::IntervalStream};
 use tokio_tungstenite::{connect_async, MaybeTlsStream, tungstenite::protocol::Message, WebSocketStream};
-
 
 pub(crate) struct WebSocketFluvioSource {
     config: WebSocketFluvioConfig,
-    // stop_tx: Option<Sender<()>>,
+}
+
+// Computes the backoff delay using an exponential strategy
+fn compute_backoff(attempt: usize, base_delay_ms: usize, max_delay_ms: usize) -> usize {
+    let exponent = 2usize.pow(attempt.min(31) as u32); // Prevent overflow, cap exponent at 2^31
+    let delay = base_delay_ms.saturating_mul(exponent);
+    delay.min(max_delay_ms)
+}
+
+async fn establish_connection(config: WebSocketFluvioConfig) -> Result<WebSocketStream<MaybeTlsStream<TcpStream>>> {
+    let mut attempt = 0;
+    if let Some(ref reconnection_policy) = config.reconnection_policy {
+        while attempt < reconnection_policy.max_retries {
+            match connect_async(config.uri.as_str()).await {
+                Ok((mut ws_stream, _)) => {
+                    info!("WebSocket connected to {}", config.uri);
+                    if let Some(message) = config.subscription_message.as_ref() {
+                        ws_stream.send(Message::Text(message.to_owned())).await?;
+                    }
+                    return Ok(ws_stream);
+                }
+                Err(e) => {
+                    error!("WebSocket connection error on attempt {}: {}", attempt, e);
+                    let delay = compute_backoff(attempt, reconnection_policy.base_delay_ms as usize, reconnection_policy.max_delay_ms as usize);
+                    sleep(Duration::from_millis(delay as u64)).await;
+                    attempt += 1;
+                }
+            }
+        }
+    }
+
+    Err(anyhow::Error::new(
+        std::io::Error::new(
+            std::io::ErrorKind::Other,
+            "Failed to establish WebSocket connection after several attempts",
+        )
+    ))
+}
+
+async fn websocket_stream<'a> (config: WebSocketFluvioConfig) -> Result<LocalBoxStream<'a, String>> {
+    let ws_stream: WebSocketStream<MaybeTlsStream<TcpStream>> = establish_connection(config).await
+    .context("Failed to establish WebSocket connection")?;
+
+    let (_write_half, read_half) = ws_stream.split();
+    let stream = read_half.filter_map(|message_result| {
+        async move {
+            match message_result {
+                Ok(message) => {
+                    match message {
+                        Message::Text(text) => {
+                            info!("Got message: {}", text);
+                            Some(text)
+                        }
+                        Message::Binary(data) => {
+                            if let Ok(text) = String::from_utf8(data) {
+                                Some(text)
+                            } else {
+                                error!("Received binary data that could not be converted to UTF-8 text");
+                                None
+                            }
+                        }
+
+                        Message::Ping(_) | Message::Pong(_) => {
+                            // upon receiving ping messages tungstenite queues pong replies automatically
+                            debug!("Received ping/pong message, connection is alive");
+                            None
+                        }
+                        Message::Close(_) => {
+                            info!("Received WebSocket Close frame");
+                            // Signal to stop and break the loop
+                            // let _ = stop_tx.send(()).await;
+                            None
+                        }
+                        _ => {
+                            // Ignore other message types
+                            None
+                        }
+                    }
+                }
+                Err(e) => {
+                    error!("WebSocket read error: {}", e);
+                    // Depending on the error you may choose to stop and close or try to reconnect
+                    Some("Error".to_string())
+                }
+            }
+        }
+    });
+    Ok(stream.boxed_local())
 }
 
 impl WebSocketFluvioSource {
@@ -22,90 +106,6 @@ impl WebSocketFluvioSource {
             config: config.clone(),
             // stop_tx: None,
         })
-    }
-
-    async fn establish_connection(&self) -> Result<WebSocketStream<MaybeTlsStream<TcpStream>>> {
-        let mut attempt = 0;
-        while let Some(reconnection_policy) = &self.config.reconnection_policy {
-            match connect_async(&self.config.uri).await {
-                Ok((mut ws_stream, _)) => {
-                    info!("WebSocket connected to {}", self.config.uri);
-                    if let Some(message) = &self.config.subscription_message {
-                        ws_stream.send(Message::Text(message.clone())).await?;
-                    }
-                    return Ok(ws_stream);
-                }
-                Err(e) => {
-                    error!("WebSocket connection error on attempt {}: {}", attempt, e);
-                    if attempt < reconnection_policy.max_retries {
-                        let delay = Self::compute_backoff(attempt, reconnection_policy.base_delay_ms as usize, reconnection_policy.max_delay_ms as usize);
-                        sleep(Duration::from_millis(delay as u64)).await;
-                        attempt += 1;
-                    } else {
-                        break;
-                    }
-                }
-            }
-        }
-
-        Err(anyhow::Error::new(
-            std::io::Error::new(
-                std::io::ErrorKind::Other,
-                "Failed to establish WebSocket connection after several attempts",
-            )
-        ))
-    }
-
-    async fn read_messages(
-        mut read: SplitStream<WebSocketStream<MaybeTlsStream<TcpStream>>>,
-        tx: Sender<String>,
-        // stop_tx: Sender<()>
-    ) {
-        while let Some(message_result) = read.next().await {
-            match message_result {
-                Ok(message) => {
-                    match message {
-                        Message::Text(text) => {
-                            info!("Got message: {}", text);
-                            if let Err(send_error) = tx.send(text).await {
-                                error!("Error sending text event to channel: {}", send_error);
-                            }
-                        }
-                        Message::Binary(data) => {
-                            if let Ok(text) = String::from_utf8(data) {
-                                if let Err(send_error) = tx.send(text).await {
-                                    error!("Error sending binary event to channel: {}", send_error);
-                                }
-                            } else {
-                                error!("Received binary data that could not be converted to UTF-8 text");
-                            }
-                        }
-                        Message::Pong(_) => {
-                            debug!("Received pong message, connection is alive");
-                        }
-                        Message::Ping(_) => {
-                            if let Err(e) = tx.send("Ping received".into()).await {
-                                error!("Failed to send ping response: {}", e);
-                            }
-                        }
-                        Message::Close(_) => {
-                            info!("Received WebSocket Close frame");
-                            // Signal to stop and break the loop
-                            // let _ = stop_tx.send(()).await;
-                            break;
-                        }
-                        _ => {
-                            // Ignore other message types
-                        }
-                    }
-                }
-                Err(e) => {
-                    error!("WebSocket read error: {}", e);
-                    // Depending on the error you may choose to stop and close or try to reconnect
-                    break;
-                }
-            }
-        }
     }
 
     // async fn send_pong(&self, write: &mut SplitSink<WebSocketStream<MaybeTlsStream<TcpStream>>, Message>) -> Result<()> {
@@ -141,113 +141,21 @@ impl WebSocketFluvioSource {
     //     }).boxed_local()
     // }
 
-
     async fn reconnect_and_run<'a> (self) -> Result<LocalBoxStream<'a, String>> {
-        // let (tx, rx) = mpsc::channel::<String>(self.config.max_message_size.unwrap_or(1024));
+        let config_clone = self.config.clone();
 
-        // let config = self.config.clone();
-
-        let ws_stream = self.establish_connection().await.context("Failed to establish WebSocket connection")?;
-        let (_write_half, read_half) = ws_stream.split();
-        // let read_tx = tx.clone();
-        // let read_stop_tx = self.stop_tx.clone().unwrap();
-        // let config_clone = config.clone();
-
-        let stream = read_half.filter_map(|message_result| {
-            async move {
-                match message_result {
-                    Ok(message) => {
-                        match message {
-                            Message::Text(text) => {
-                                info!("Got message: {}", text);
-                                Some(text)
-                            }
-                            Message::Binary(data) => {
-                                if let Ok(text) = String::from_utf8(data) {
-                                    Some(text)
-                                } else {
-                                    error!("Received binary data that could not be converted to UTF-8 text");
-                                    None
-                                }
-                            }
-                            Message::Pong(_) => {
-                                debug!("Received pong message, connection is alive");
-                                None
-                            }
-                            Message::Ping(_) => {
-                                debug!("Received ping message, should pong");
-                                None
-                            }
-                            Message::Close(_) => {
-                                info!("Received WebSocket Close frame");
-                                // Signal to stop and break the loop
-                                // let _ = stop_tx.send(()).await;
-                                None
-                            }
-                            _ => {
-                                // Ignore other message types
-                                None
-                            }
-                        }
-                    }
-                    Err(e) => {
-                        error!("WebSocket read error: {}", e);
-                        // Depending on the error you may choose to stop and close or try to reconnect
-                        None
-                    }
+        let repeated_websocket = Box::pin(async_stream::stream! {
+            loop {
+                let mut ws_stream = websocket_stream(config_clone.clone())
+                .await
+                .unwrap();
+                while let Some(item) = ws_stream.next().await {
+                    yield item;
                 }
             }
         });
-        // let read_handle = tokio::spawn(async move {
-        //     WebSocketFluvioSource::read_messages(
-        //         read_half,
-        //         read_tx,
-        //         // read_stop_tx,
-        //     )
-        //     .await;
-        // });
-
-        // let write_stop_tx = self.stop_tx.clone().unwrap();
-        // let ping_handle = tokio::spawn(async move {
-        //     WebSocketFluvioSource::ping_interval_task(
-        //         write_half,
-        //         config_clone,
-        //         // write_stop_tx
-        //     ).await;
-        // });
-
-        // Wait for any of the tasks to finish. If either finishes, we have to reconnect.
-        // tokio::select! {
-        //     _ = read_handle => {
-        //         error!("WebSocket read task ended, reconnecting...");
-        //     },
-        //     // _ = ping_handle => {
-        //     //     error!("WebSocket ping task ended, reconnecting...");
-        //     // },
-        // };
-
-        // // Should not reconnect if reconnection policy is not defined.
-        // let reconnect = config.reconnection_policy.as_ref().map(|policy| policy.enabled).unwrap_or(false);
-        // debug!("Reconnect policy is enable={}", reconnect);
-        // if !reconnect {
-        //     debug!("Reconnection disabled, ending the source.");
-        //     //break;
-        // }
-
-        // // Apply the backoff strategy before attempting to reconnect.
-        // if let Some(ref policy) = config.reconnection_policy {
-        //     let delay = Self::compute_backoff(0 /* reset or implement retry counter */, (policy.base_delay_ms as u64).try_into().unwrap(), (policy.max_delay_ms as u64).try_into().unwrap());
-        //     sleep(Duration::from_millis(delay.try_into().unwrap())).await;
-        // }
-
-        Ok(stream.boxed_local())
-    }
-
-    // Computes the backoff delay using an exponential strategy
-    fn compute_backoff(attempt: usize, base_delay_ms: usize, max_delay_ms: usize) -> usize {
-        let exponent = 2usize.pow(attempt.min(31) as u32); // Prevent overflow, cap exponent at 2^31
-        let delay = base_delay_ms.saturating_mul(exponent);
-        delay.min(max_delay_ms)
+        
+        Ok(repeated_websocket.boxed_local())
     }
 }
 
